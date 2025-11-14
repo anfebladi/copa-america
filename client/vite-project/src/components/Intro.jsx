03036395fc411a2c1f81788667cce334ce9aa194
anfebladi <anfebladi4@gmail.com>
2025-11-13 21:11:13 -0800

    made it responsive

--- client/vite-project/src/components/Intro.jsx
+++ client/vite-project/src/components/Intro.jsx
@@ -6,14 +6,15 @@ export default function Intro() {
 
 
   return (
+
     <div className="row h-100 align-items-center justify-content-center mx-5">
-        <div className="col-5 d-flex justify-content-center mt-5">
-          <Link to="/teams" className=" hover-glow display-1 text-start fw-bolder text-decoration-none text-white mt-5">
-            Build your dream <br></br> team now
-          </Link>
+      <div className="col-12 col-lg-5 d-flex justify-content-center mt-5">
+        <Link to="/teams" className="hover-glow display-1 text-start fw-bolder text-decoration-none text-white mt-5">
+          Build your dream <br></br> team now
+        </Link>
       </div>
-      <div className="col-5 d-flex justify-content-center mt-5 ">
-        <img className="mt-5 hover-glow" src="https://i.imgur.com/tYFkSng.png"></img>
+      <div className="col-12 col-lg-5 d-flex justify-content-center  mt-md-5">
+        <img className="mt-md-5 hover-glow" src="https://i.imgur.com/tYFkSng.png"></img>
       </div>
     </div>
   );
--- client/vite-project/src/components/Players.jsx
+++ client/vite-project/src/components/Players.jsx
@@ -30,41 +30,34 @@ export default function Players() {
 
     return (
         <> 
-             
-                 
+            <div className="row">
+                <h1 className="text-center mt-5 mb-5 display-3 fw-bold"> Copa America 2024 Squad: </h1>
+            </div>
+            <div className="row">
+                <div className="col-12 justify-content-center d-flex">
+                    <input placeholder="Enter player's name" onChange={(e)=> setSearch(e.target.value)} className="p-2 pe-5 ps-5 mt-3 mb-3"></input>
+                </div>
+            </div>
+
+            <div className="row justify-content-center d-flex">
+
+                {countries.players.filter(player => search === '' || player.name.toLowerCase().includes(search.toLowerCase())).map( player => (
+                <div className="col-12 col-md-6 col-lg-4 col-xl-3 ms-xl-5 me-xl-5 bg-white mt-2 mt-md-5 mb-5 rounded hover-zoom">
+                    <div className="bg-info m-3">
+                    <p className="text-dark text-center fs-2 fw-bold mb-4"> {player.name} </p>
                 
-                    <div className="row">
-                        <h1 className="text-center mt-5 mb-5 display-3 fw-bold"> Copa America 2024 Squad: </h1>
+                    <p className="text-dark text-center fs-3 fw-bold"> Position:</p>
+                    <p className="text-dark text-center fs-2 fw-bold mb-3">{player.position}</p>
+
+                    <p className="text-dark text-center fs-3 fw-bold"> Goals:</p>
+                    <p className="text-dark text-center fs-2 fw-bold mb-3">{player.goals}</p>
+
+                    <p className="text-dark text-center fs-3 fw-bold"> Assists:</p>
+                    <p className="text-dark text-center fs-2 fw-bold mb-3">{player.assist}</p>
                     </div>
-                    <div className="row">
-                        <div className="col-12 justify-content-center d-flex">
-                            <input placeholder="Enter player's name"  onChange={(e)=> setSearch(e.target.value)}className="p-2 pe-5 ps-5 mt-3 mb-3"></input>
-                        </div>
-                    </div>
-
-                    <div className="row justify-content-center d-flex">
-
-                        {countries.players.filter(player => search === '' || player.name.toLowerCase().includes(search.toLowerCase())).map( player => (
-                            <div className="col-3 ms-5 me-5 bg-white mt-5 mb-5  rounded hover-zoom">
-                                <div className="bg-info m-3">
-                                    <p className="text-dark text-center fs-2 fw-bold mb-4"> {player.name} </p>
-                                    
-                                    <p className="text-dark text-center fs-3 fw-bold"> Position:</p>
-                                    <p className="text-dark text-center fs-2 fw-bold mb-3">{player.position}</p>
-
-                                    <p className="text-dark text-center fs-3 fw-bold"> Goals:</p>
-                                    <p className="text-dark text-center fs-2 fw-bold mb-3">{player.goals}</p>
-
-                                    <p className="text-dark text-center fs-3 fw-bold"> Assists:</p>
-                                    <p className="text-dark text-center fs-2 fw-bold mb-3">{player.assist}</p>
-
-                                    
-                                </div>
-                            </div>
-                            
-                        ))}
-
-                    </div>
+                </div>
+                ))}
+            </div>
                     
                         
                     
