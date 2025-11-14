0ae5ac5dff60fe96addf976a67cd13ebe0364f95
anfebladi <anfebladi4@gmail.com>
2025-11-13 21:55:30 -0800

    changed a file name

--- client/vite-project/src/components/Teams.jsx
+++ client/vite-project/src/components/Teams.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { useEffect, useState} from "react"
 import { Link } from "react-router-dom"
-import  './teams.css';
+import  './Teams.css';
 
 
 export default function Teams() {
